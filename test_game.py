b4fcfbdeee98e34ccd189bd3e70ff2c541a3cafe
dykim <dykim4225@gmail.com>
2025-07-31 14:18:00 +0900

    [feature] page 60
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_game.py
+++ test_game.py
@@ -1,5 +1,6 @@
 import pytest
 from game import Game
+
 from game_result import GameResult
 
 
@@ -12,9 +13,11 @@ def assert_type_error(game, guess_number):
     with pytest.raises(TypeError):
         game.guess(guess_number)
 
+
 @pytest.mark.parametrize("invalid_input", [None, "12", "1234", "123s", "121"])
 def test_exception_when_input_type_is_wrong(game, invalid_input):
     assert_type_error(game, invalid_input)
+
 
 def test_return_solved_result_if_matched_number(game):
     game.question = "123"
@@ -23,4 +26,14 @@ def test_return_solved_result_if_matched_number(game):
     assert result is not None
     assert result.solved == True
     assert result.strikes == 3
+    assert result.balls == 0
+
+
+def test_return_solved_result_if_unmatched_number(game):
+    game.question = "123"
+    result: GameResult = game.guess("456")
+
+    assert result is not None
+    assert result.solved == False
+    assert result.strikes == 0
     assert result.balls == 0
