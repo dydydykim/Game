0b4cc953825be138a84d1186ccaca550f7543dc1
dykim <dykim4225@gmail.com>
2025-07-31 13:58:47 +0900

    [feature] page 40
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_game.py
+++ test_game.py
@@ -16,4 +16,5 @@ def test_exception_when_input_type_is_wrong(game):
     assert_type_error(game, None)
     assert_type_error(game, "12")
     assert_type_error(game, "1234")
+    assert_type_error(game, "123s")
 
