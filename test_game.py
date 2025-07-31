0514c8e41a2410483c25e18e6b9d4bc34f127207
dykim <dykim4225@gmail.com>
2025-07-31 13:51:36 +0900

    [feature] page 27
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_game.py
+++ test_game.py
@@ -1,4 +1,7 @@
- import pytest
+from game import Game
+import pytest
 
- def test_game():
-     pass
+def test_game():
+     game = Game()
+     with pytest.raises(TypeError):
+         game.guess(None)
