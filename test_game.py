ce2e03bd61ac6e0b525df3c35526b28046679b16
dykim <dykim4225@gmail.com>
2025-07-31 14:06:31 +0900

    [refactor] page 49
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_game.py
+++ test_game.py
@@ -1,21 +1,16 @@
 from game import Game
 import pytest
 
+
 @pytest.fixture
 def game():
     return Game()
 
-def assert_type_error(game, guessNumber):
-    try:
-        game.guess(guessNumber)
-        pytest.fail()
-    except TypeError:
-        pass
 
-def test_exception_when_input_type_is_wrong(game):
-    assert_type_error(game, None)
-    assert_type_error(game, "12")
-    assert_type_error(game, "1234")
-    assert_type_error(game, "123s")
+def assert_type_error(game, guess_number):
+    with pytest.raises(TypeError):
+        game.guess(guess_number)
 
-    assert_type_error(game, "121")
+@pytest.mark.parametrize("invalid_input", [None, "12", "1234", "123s", "121"])
+def test_exception_when_input_type_is_wrong(game, invalid_input):
+    assert_type_error(game, invalid_input)
